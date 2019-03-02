9cf36fbceb071d91e6d2659e27c9805dbe8194b0
ykhedher <ykhedher@users.noreply.github.com>
2019-03-02 22:35:41 +0000

    20190302T223511Z

--- mario.c
+++ mario.c
@@ -3,28 +3,22 @@
 
 int main(void)
 {
-  int Height;
+int Height;
     do
     {
         Height = get_int("Height: ");
     } 
     while (Height > 8 || Height < 1);
-    
-    int Stored = Height;
-    
-    printf("Stored: %i\n", Stored);
-    
     for (int i = 1; i <= Height; i++)
     {
         for (int k = 0; k < Height-i; k++)
-        {
+          {
             printf(" ");    
-        }
+          }
             for (int j = 1; j <= i; j++)
-            {
+              {
                 printf("#");
-          
-            }
+              }
         printf("\n");
     }
 }
