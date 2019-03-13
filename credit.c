2a64e628dc30ddf04e10c690aacab3d39863ac1f
ykhedher <ykhedher@users.noreply.github.com>
2019-03-13 20:21:08 +0000

    20190313T201057Z

--- credit.c
+++ credit.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <cs50.h>
-int main(void)
+#include <math.h>
+//prototype of the function
+int func(long credit, int count);
+int main(void) 
 {
     long Credit = get_long("Number: ");
     int count = 0;
@@ -10,18 +13,21 @@ int main(void)
     int MCARD = (int)(Credit / 100000000000000);
     //FIRST DIGIT IN CASE 13 DIGITS
     int VISA = (int)(Credit / 1000000000000);
+    long copy = Credit;
+    //count how many digits
     while (Credit > 1)
     {
         Credit /= 10;
         count++;
     }
+    int isValid = func(copy, count); 
     //INVALID
-    if (count != 13 && count != 15 && count != 16)
+    if (isValid)     
     {
         printf("INVALID\n");
     }
     //american-express
-    if (count == 15 && (AMEX == 37 || AMEX == 34))
+    else if (count == 15 && (AMEX == 37 || AMEX == 34))
     {
         printf("AMEX\n");
     }
@@ -36,8 +42,61 @@ int main(void)
     {
         printf("VISA\n");
     }
-       
-
-    
-    
+    else 
+    {
+        printf("INVALID\n");
+    }
+      
 }
+
+// function 
+int func(long credit, int count)
+{ 
+    long p = pow(10, count - 1);
+    int sum = 0;
+    for (int i = 0; i < count; i++)
+    {
+        // extract the ith digit
+        int k = credit / p;
+        // check if the digit is odd or even
+        if (i % 2 == 0)
+        {
+            k *= 2;
+            // if it's higher than 10 exp 12 = 1 + 2
+            if (k > 9)
+            {
+                sum += ((int) k / 10) + (k % 10);
+            }
+            // if it's not higher than 9 add it to the sum directly
+            else
+            {
+                sum += k;
+            }
+                
+        }
+        // if it's not even add it to the sum without doubling it
+        else 
+        {
+            sum += k;
+        }
+        //change the new value of credit and p(diviser)
+        credit %= p;
+        p /= 10;   
+    }
+     
+    if (sum % 10 == 0)
+    {
+        //return 0 means it's valid
+        return 0;
+    }
+    
+            
+    else
+    {
+        //return 1 means it's not valid
+        return 1;
+    }
+        
+}
+
+
