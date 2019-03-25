ef4cef4e2074b56a43ffff5c1f324c8166bedda8
ykhedher <ykhedher@users.noreply.github.com>
2019-03-25 04:10:50 +0000

    20190325T041036Z

--- caesar.c
+++ caesar.c
@@ -12,7 +12,7 @@ int main(int argc, string argv[])
     if (argc != 2)
     {
         //printf("Usage: ./caesar key\n");
-        exit(0);
+        exit(1);
       
     }          
     // case the number of arguments is 2 and arg 2 is string or number + characters
